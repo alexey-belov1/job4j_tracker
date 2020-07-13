4288f8fa3426443b05fbef701f6206d14b2126e0
alexey-belov1 <veshousis1@gmail.com>
2020-07-13 20:02:40 +0500

    2. Сканиварование проекта

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -1,14 +1,11 @@
 package ru.job4j.di;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.job4j.tracker.ConsoleInput;
 
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(ConsoleInput.class);
-        context.register(StartUI.class);
+        context.scan("ru.job4j");
         context.refresh();
 
         StartUI ui = context.getBean(StartUI.class);
